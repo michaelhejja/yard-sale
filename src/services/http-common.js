8d24cec4de3b65c7b89933aab15ca78972489347
Michael Hejja <mhejja@us3cmam0vxh0y.myfiosgateway.com>
2025-02-07 15:37:06 -0500

    update server location

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export default axios.create({
-  baseURL: "http://localhost:8082/",
+  baseURL: "https://yard-sale-server-6f9e6e59ab49.herokuapp.com/",
   headers: {
     Accept: 'application/json',
     "Content-Type": "application/json"
